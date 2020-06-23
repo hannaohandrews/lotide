eb908421d61f4bf473505602bc14f9800e906663
Hanna Oh Andrews <hannaohandrews@gmail.com>
2020-06-23 15:28:40 +0000

    tail committed

---
+++ tail.js
@@ -0,0 +1,34 @@
+
+
+var finalarray = []
+
+function tail (array) {
+	for (var i = 1 ; i < array.length ; i++) {
+		finalarray.push(array[i]);
+	} return (finalarray)
+}
+
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log( 'Assertion Passed:' + actual + '===' + expected )
+  } else {
+    console.log ( 'Assertion Failed: ' + actual + '!==' + expected )
+  }
+};
+
+
+// Test Case 1: Check the returned array elements
+// FUNCTION IMPLEMENTATION
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse");
+ // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case 2: ...
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
