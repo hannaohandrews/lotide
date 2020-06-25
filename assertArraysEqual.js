de5851354257ca74434625b27551bf9d280200da
hannaohandrews <hannaohandrews@gmail.com>
2020-06-25 01:33:00 +0000

    done

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,39 @@
 // FUNCTION IMPLEMENTATION
 
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log( `✅✅✅ Assertion Passed: ${actual} ===  ${expected}` )
+  } else {
+    console.log ( `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}` )
+  }
+}
+
 const eqArrays = (arrayOne, arrayTwo) => {
- 
-if ((arrayOne[0] === arrayTwo[0]) && (arrayOne[1] === arrayTwo[1]) && (arrayOne[2] === arrayTwo[2])) {
-  return true
- } else {
-   return false
- } 
+  let outcome = "";
+
+  for (let i = 0 ;i < arrayOne.length ; i++) {
+    if (arrayOne[i] === arrayTwo[i]){
+      outcome = true 
+    } else {
+      outcome = false 
+    }
+  } return outcome
 }
 
-const assertArraysEqual = () => { 
-  true ? console.log ( 'Assertion Passed') : console.log('Assertion Failed')
+const assertArraysEqual = function(arrayOne, arrayTwo){
+  let outcome ;
+
+  for (let i = 0 ; i < arrayOne.length ; i++) {
+    if (arrayOne[i] === arrayTwo[i]){
+      outcome = `✅✅✅ Assertion Passed: ${actual} ===  ${expected}` 
+    } else {
+      outcome = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
+    }
+    
+  }return output
 }
 
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => should PASS
