addbddbaa8df563ce0efd79c98a259b13580bd44
hannaohandrews <hannaohandrews@gmail.com>
2020-06-28 22:37:32 +0000

    findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(object, title) {
+  let keys = Object.keys(object);
+  
+  for (let i of keys) {
+    if (object[i] === title) {
+      let finalKey = i;
+      return (finalKey);
+    }
+  }
+};
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
