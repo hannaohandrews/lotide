7ddad4a7a8cf24684938730043f3c9d40ea11644
hannaohandrews <hannaohandrews@gmail.com>
2020-06-29 23:30:00 +0000

    done

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,38 +1,34 @@
 // FUNCTION IMPLEMENTATION
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log( `✅✅✅ Assertion Passed: ${actual} ===  ${expected}` )
-  } else {
-    console.log ( `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}` )
-  }
-}
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log( `✅✅✅ Assertion Passed: ${actual} ===  ${expected}` )
+//   } else {
+//     console.log ( `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}` )
+//   }
+// }
 
 const eqArrays = (arrayOne, arrayTwo) => {
   let outcome = "";
 
-  for (let i = 0 ;i < arrayOne.length ; i++) {
-    if (arrayOne[i] === arrayTwo[i]){
-      outcome = true 
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] === arrayTwo[i]) {
+      outcome = true;
     } else {
-      return false 
+      return false;
     }
-  } return outcome
-}
-
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-
-    if (eqArrays(arrayOne,arrayTwo)) {
-       return `✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`;
-    } else {
-      return `🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`;
-    } 
+  } return outcome;
 };
 
-let answer = eqArrays([1, 2, 3],[1, 2, 3]) // => true
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne,arrayTwo)) {
+    return `✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`;
+  } else {
+    return `🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`;
+  }
+};
 
-console.log(assertArraysEqual([1, 2, 3],[1, 2, 3]) );
-
+console.log(assertArraysEqual([1, 2, 3],[1, 2, 3]));
 
 module.exports = assertArraysEqual;
 
