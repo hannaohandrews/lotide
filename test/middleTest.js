5f0fc0c789a4c4a643958c2af362157a8f35c1ab
hannaohandrews <hannaohandrews@gmail.com>
2020-06-30 04:37:40 +0000

    done

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,3 +1,16 @@
-const middle = require('../middle.js');
 
-module.exports = middle;
+
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe("#middle", () => {
+
+  it("returns 6 for [5,6,5]", () => {
+    assert.deepEqual(middle([5,6,5]), [6]); 
+  });
+
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+});
